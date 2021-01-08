1ad7ae4f8db98293e35da848177438f8b296fb10
sanjay.sharma <sanjay.sharma@gameskraft.in>
2021-01-08 11:55:08 +0530

    create your own stack✨

--- Stacks.js
+++ Stacks.js
@@ -18,10 +18,10 @@ function b() {
 // functions: push, pop, peek, length
 
 var letters = []; // this is our stack
-var rletters = []; // this is our stack
 
 
 var word = "racecar";
+// var word = prompt("Please enter your name", "Harry Potter") || "racecar";
 
 var rword = "";
 
@@ -35,8 +35,6 @@ for(var i = 0; i < word.length; i++) {
 for(var i = 0; i < word.length; i++) {
     var xoxo = letters.pop()
     rword += xoxo; // ReSanjay
-    // rletters.push(letters.pop())
-    // console.log(letters.pop())
 }
 
 if (rword === word) {
@@ -45,4 +43,51 @@ if (rword === word) {
  else {
     console.log(word + " is not a palindrome.");
  }
+
+ // Creates a stack
+var Stack = function() {
+    this.count = 0;
+    this.storage= {};
+
+    // Adds a value onto the end of the stack
+    this.push = function(value) {
+        this.storage[this.count] = value;
+        this.count++; 
+    }
+
+    // Removes and returns the value at the end of the stack
+    this.pop = function() {
+        if(this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    // Returns the value at the end of the stack
+    this.peek = function() {
+        return this.storage[this.count-1];
+    }
+
+    this.size = function() {
+        return this.count;
+    }
+}
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push("HarryPotter");
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+
 
